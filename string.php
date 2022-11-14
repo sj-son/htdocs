9876ac85f4ce1613f28aec2a5911884d99f5a7f6
SJSON <sjson666@gmail.com>
2022-11-14 20:17:35 +0900

    06

---
+++ string.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>String</title>
+  </head>
+  <body>
+    <h1>String & String Operrator</h1>
+    <?php
+     echo "Hello \"W\"ord";
+    ?>
+    <h2>Concatenation Operrator</h2>
+    <?php
+    echo "Hello" . "world";
+    ?>
+    <h2>String length function</h2>
+    <?php
+    echo strlen("Hello world");
+    ?>
+  </body>
+</html>
---
+++ variable.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Variable</title>
+  </head>
+  <body>
+    <h1>Variable</h1>
+    <?php
+    $name = "leezche";
+    echo "Lorem ipsum dolor ".$name." sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ".$name." veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit ".$name." in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+     ?>
+  </body>
+</html>
