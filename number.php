79169bb110151753628ccf322158d21eab7e1620
SJSON <sjson666@gmail.com>
2022-11-15 08:30:32 +0900

    07

---
+++ number.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Number & Operator</h1>
+    <h2>1+1</h2>
+    <?php
+    echo 1 + 1;
+    ?>
+    <h2>2-1</h2>
+    <?php
+    echo 2 - 1;
+    ?>
+    <h2>2*2</h2>
+    <?php
+    echo 2*2;
+    ?>
+    <h2>4/2</h2>
+    <?php
+    echo 4/2;
+    ?>
+  </body>
+</html>
